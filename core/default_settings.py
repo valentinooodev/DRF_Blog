ad0d8028f64f48a933e1a6173ea182f5328270e6
= <=>
2021-11-18 15:57:38 +0700

    fix conflict

--- core/default_settings.py
+++ core/default_settings.py
@@ -128,7 +128,6 @@ USE_TZ = True
 
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static/'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static/'),
@@ -154,4 +153,3 @@ REST_FRAMEWORK = {
     #     'rest_framework.authentication.BasicAuthentication',
     # ),
 }
-
